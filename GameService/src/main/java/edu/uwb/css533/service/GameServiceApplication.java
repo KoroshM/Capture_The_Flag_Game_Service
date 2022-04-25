46dbb3ffddeb2b655ff875c45dca7dc42a865108
Nayana Yeshlur <nayanayeshlur@Nayanas-MacBook-Pro.local>
2022-04-25 11:07:33 -0700

    revised

---
+++ GameService/src/main/java/edu/uwb/css533/service/GameServiceApplication.java
@@ -0,0 +1,38 @@
+package edu.uwb.css533.service;
+
+import edu.uwb.css533.service.resources.UserResource;
+import io.dropwizard.Application;
+import io.dropwizard.jdbi3.JdbiFactory;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.jdbi.v3.core.Jdbi;
+
+public class GameServiceApplication extends Application<GameServiceConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new GameServiceApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "GameService";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<GameServiceConfiguration> bootstrap) {
+        // TODO: application initialization
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(final GameServiceConfiguration configuration,
+                    final Environment environment) {
+        final JdbiFactory factory = new JdbiFactory();
+        final Jdbi jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
+        final UserDao user_dao = jdbi.onDemand(UserDao.class);
+        //final GameDao game_dao = jdbi.onDemand(GameDao.class);
+        environment.jersey().register(new UserResource(jdbi, user_dao));
+        //environment.jersey().register(new GameResource(jdbi, game_dao));
+    }
+
+}
