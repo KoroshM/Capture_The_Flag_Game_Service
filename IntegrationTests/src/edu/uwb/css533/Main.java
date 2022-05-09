cd2c3c880ade6e43a505a26c2ef1b326a9dc10ce
Nayana Yeshlur <nayanayeshlur@Nayanas-MacBook-Pro.local>
2022-05-09 10:38:58 -0700

    integrationtests

--- IntegrationTests/src/edu/uwb/css533/Main.java
+++ IntegrationTests/src/edu/uwb/css533/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
 
 
-        //Test 1
+        //Test 1 - normal path
         String username = "korosh"; //must be new user
         String password = "password";
 
@@ -50,7 +50,7 @@ public class Main {
 
 
 
-        //Test 2
+        //Test 2 - error condition
         username = "nayana"; //must be existing user
         password = "password";
 
@@ -80,7 +80,7 @@ public class Main {
         }
 
 
-        //Test 3
+        //Test 3 - normal path
         username = "nayana"; //must be existing user
         password = "password"; //must be correct password
 
@@ -101,6 +101,176 @@ public class Main {
                     System.out.println("Test 3 is successful.");
                 } else {
                     System.out.println("Test 3 is not successful.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (URISyntaxException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Test 4 - error condition
+        username = "nayana"; //must be existing user
+        password = "123"; //must be incorrect password
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://localhost:8080/capture_the_flag/login?username=" + username
+                            + "&password=" + password))
+                    .GET()
+                    .timeout(Duration.ofSeconds(10))
+                    .build();
+            try {
+                String response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body();
+                System.out.println(response);
+
+                int check = response.compareTo("Incorrect Password.");
+
+                if(check == 0) {
+                    System.out.println("Test 4 is successful.");
+                } else {
+                    System.out.println("Test 4 is not successful.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (URISyntaxException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Test 5 - error condition
+        username = "palmer"; //must be user that does not currently exist
+        password = "password";
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://localhost:8080/capture_the_flag/login?username=" + username
+                            + "&password=" + password))
+                    .GET()
+                    .timeout(Duration.ofSeconds(10))
+                    .build();
+            try {
+                String response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body();
+                System.out.println(response);
+
+                int check = response.compareTo("User does not exist.");
+
+                if(check == 0) {
+                    System.out.println("Test 5 is successful.");
+                } else {
+                    System.out.println("Test 5 is not successful.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (URISyntaxException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Test 6 - normal path
+        int userId = 0; //user is logged in and is not part of another active session
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://localhost:8080/capture_the_flag/new_room?user_id=" + userId))
+                    .GET()
+                    .timeout(Duration.ofSeconds(10))
+                    .build();
+            try {
+                String response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body();
+                System.out.println(response);
+
+                int check = response.compareTo("Player joined new session.");
+
+                if(check == 0) {
+                    System.out.println("Test 6 is successful.");
+                } else {
+                    System.out.println("Test 6 is not successful.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (URISyntaxException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Test 7 - error condition
+        userId = 1; //user is logged in and is part of another active session
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://localhost:8080/capture_the_flag/new_room?user_id=" + userId))
+                    .GET()
+                    .timeout(Duration.ofSeconds(10))
+                    .build();
+            try {
+                String response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body();
+                System.out.println(response);
+
+                int check = response.compareTo("Player cannot join new session.");
+
+                if(check == 0) {
+                    System.out.println("Test 7 is successful.");
+                } else {
+                    System.out.println("Test 7 is not successful.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (URISyntaxException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Test 8 - normal path
+        userId = 1; //user is logged in, session exists, and session is not full
+        int sessionId = 123;
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://localhost:8080/capture_the_flag/join_room?user_id=" + userId
+                            + "&session_id=" + sessionId))
+                    .GET()
+                    .timeout(Duration.ofSeconds(10))
+                    .build();
+            try {
+                String response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body();
+                System.out.println(response);
+
+                int check = response.compareTo("Player joined " + sessionId + ".");
+
+                if(check == 0) {
+                    System.out.println("Test 8 is successful.");
+                } else {
+                    System.out.println("Test 8 is not successful.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (URISyntaxException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Test 9 - error condition
+        userId = 1; //user is logged in, session exists, and session is not full
+        int sessionId = 123;
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://localhost:8080/capture_the_flag/join_room?user_id=" + userId
+                            + "&session_id=" + sessionId))
+                    .GET()
+                    .timeout(Duration.ofSeconds(10))
+                    .build();
+            try {
+                String response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body();
+                System.out.println(response);
+
+                int check = response.compareTo("Player joined " + sessionId + ".");
+
+                if(check == 0) {
+                    System.out.println("Test 8 is successful.");
+                } else {
+                    System.out.println("Test 8 is not successful.");
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
